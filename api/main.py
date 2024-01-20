30bdc51fc7636ef28d30dd6c2791c68e3d9ad3b9
DavidMCKim <kimmincang96@gmail.com>
2024-01-21 01:22:13 +0900

    [FIX]fastapi 소스 수정

--- api/main.py
+++ api/main.py
@@ -1,40 +1,71 @@
-from app.router import mysql
 import configparser
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-import os
-import uvicorn
-    
+from http import server
+import pymysql as mysql
+from fastapi import APIRouter, Request
+from app.database.mongo_connector import MONGO
 
-def create_app():
-    app = FastAPI()
+config = configparser.ConfigParser()
+config.read('config.ini')
 
-    app.include_router(mysql.router)
+db = MONGO()
 
-    return app
+router = APIRouter(prefix='/db/mongo')
 
+@router.post('/GetServerInfo')
+async def get_server_info(request: Request):
+    server_info = {
+        'channel_code' : '-1'
+    }
+    try:
+        req = await request.json()
+        hostname = req['hostname']
+        query = f'''
+                    select ChannelCode
+                    from tb_Server_Info
+                    where ServerName = '{hostname}'
+                '''
+        data = db.select(query)
+        if data:
+            channel_cdoe = data[0]
 
-app = create_app()
+            server_info = {
+                'channel_code' : f'{channel_cdoe}'
+            }
+    except Exception as e:
+        print(e)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=['*'],
-    allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*'],
-    expose_headers=[]
-)
+    return server_info
 
-def main():
-    uvicorn.run(
-        app="main:app",
-        host=config['APP']['host'],
-        port=int(config['APP']['port']),
-        reload=True
-    )
+@router.post('/RecommandRecentPlace')
+async def get_server_info(request: Request):
+    server_info = {
+        'address_main'    : '-1',
+        'address_detail'  : '-1',
+        'place_name'      : '-1',
+        'star_score'      : '-1',
+        'keyword'         : '-1'
+    }
+    try:
+        startdate = request['startdate']
+        enddate = request['enddate']
+        req = await request.json()
+        hostname = req['hostname']
+        query = f'''
+                    select Address_main, Address_detail, Place_Name, Star_Scroe, Keyword
+                    from Map_Naver_Place
+                    where RegDate between '{startdate}' and '{enddate}'
+                    order by RegDate desc
+                '''
+        data = db.select(query)
+        if data:
+            server_info = {
+                'address_main'    : f'{data[0]}',
+                'address_detail'  : f'{data[1]}',
+                'place_name'      : f'{data[2]}',
+                'star_score'      : f'{float(data[3])}',
+                'keyword'         : f'{data[4]}'
+            }
+    except Exception as e:
+        print(e)
 
-if __name__ == "__main__":
-    # 개 불러오기
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    main()
+    return server_info
